ea90deb184f6c8005232a12aa7e02326a64bf1b5
Eduardo R. Oliveira <83559192+eeduardoliveira@users.noreply.github.com>
2026-06-19 01:49:59 +0100

    Implement comprehensive logging improvements with SLF4J - src/main/java/com/example/demo/controller/UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.model.User;
 import com.example.demo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("usuario")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @PostMapping(path = {"sign-in"})
     public ResponseEntity sign(@RequestBody User user) throws Exception {
+        logger.info("Received sign-in request for username: {}", user.getUsername());
+        logger.debug("User registration request details: name='{}'", user.getName());
+        
         userService.signIn(user);
-        return  ResponseEntity.ok(user);
+        
+        logger.info("User sign-in successful: {}", user.getUsername());
+        return ResponseEntity.ok(user);
     }
 }
